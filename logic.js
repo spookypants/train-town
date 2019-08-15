e1349f9d4e0533cb91fee1083fe80469a70433b3
Gabe <42557432+spookypants@users.noreply.github.com>
2019-08-15 17:03:57 -0500

    logic

--- logic.js
+++ logic.js
@@ -8,3 +8,80 @@ var firebaseConfig = {
     // messagingSenderId: "453213552728",
     // appId: "1:453213552728:web:8d5e02e146066b25"
   };
+
+  firebase.itializeApp(firebaseConfig);
+
+  var trainData = firebase.database();
+
+  $("#add-train-btn").on("click", function(event) {
+      event.preventDefault();
+
+      var trainName = $("#train-name-input")
+      .val()
+      .trim();
+      var destination = $("#destination-input")
+      .val()
+      .trim();
+      var firstTrain = $("#first-train-input")
+      .val()
+      .trim();
+      var frequency = $("#frequency-input")
+      .val()
+      .trim();
+
+      var newTrain = {
+          name: trainName,
+          destination: destination,
+          firstTrain: firstTrain,
+          frequency: frequency
+      };
+
+      alert("Train successfull added!");
+
+      $("#train-name-input").val("");
+      $("#destination-input").val("");
+      $("#first-train-input").val("");
+      $("#frequency-input").val("");
+  });
+
+  trainData.ref().on("child_added", function(childSnapshot, prevChildkey) {
+      console.log(childSnapshot.val());
+
+      var tName = childSnapshot.val().name;
+      var tDestination = childSnapshot.val().destination;
+      var tFrequency = childSnapshot.val().frequency;
+      var tFirstTrain = childSnapshot.val().firstTrain;
+
+      var timeArr = tFirstTrain.splt(":");
+      var trainTime = moment()
+        .hours(timeArr[0])
+        .minutes(timeArr[1]);
+      var maxMoment = moment.max(moment(), trainTime);
+      var tMinutes;
+      var tArrival;
+
+
+      if (maxMoment === trainTime) {
+          tArrival = trainTime.format("hh:mm A");
+          tMinutes = trainTime.diff(moment(), "minutes");
+      } else {
+          var differenceTimes = moment().diff(trainTime, "minutes");
+          var tRemainder = differenceTimes % tFrequency;
+          tMinutes = tFrequency - tRemainder;
+          tArrival = mmoment()
+            .add(tMinutes, "m")
+            .format("hh:mm A");
+      }
+      console.log("tMinutes: ", tMinutes);
+      console.log("tArrival: ", tArrival);
+
+      $("#train-table > tbody").append(
+          $("<tr>").append(
+              $("<td>").text(tName),
+              $("<td>").text(tDestination),
+              $("<td>").text(tFrequency),
+              $("<td>").text(tArrival),
+              $("<td>").text(tMinutes)
+          )
+      )
+  })

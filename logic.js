9547706a76fa729292d19ea8142372345c3c7b1f
Gabe <42557432+spookypants@users.noreply.github.com>
2019-08-15 16:53:57 -0500

    initial commit

---
+++ logic.js
@@ -0,0 +1,10 @@
+// firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyC0PvlYl1zezezf56r5Alxk5Lfav2-qt3c",
+    authDomain: "train-town.firebaseapp.com",
+    databaseURL: "https://train-town.firebaseio.com",
+    projectId: "train-town",
+    storageBucket: "",
+    // messagingSenderId: "453213552728",
+    // appId: "1:453213552728:web:8d5e02e146066b25"
+  };
